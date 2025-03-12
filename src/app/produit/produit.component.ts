515bf17b51ed189fde9c9157fb84a998ffd2d0dc
stmiinfo <stmi.informatique@gmail.com>
2025-03-12 14:57:41 +0100

    added produits

---
+++ src/app/produit/produit.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-produit',
+  imports: [],
+  templateUrl: './produit.component.html',
+  styleUrl: './produit.component.css'
+})
+export class ProduitComponent {
+
+}
