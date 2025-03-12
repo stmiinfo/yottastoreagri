b2cf56045667b735fd656aaa0ec448fbb6ddbc52
stmiinfo <stmi.informatique@gmail.com>
2025-03-12 11:54:09 +0100

    map+contact

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,36 +1,12 @@
-import { AfterViewInit, Component, ElementRef, ViewChild, Inject, PLATFORM_ID } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
   imports: [],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrls: ['./contact.component.css']
 })
-export class ContactComponent implements AfterViewInit {
-  @ViewChild('map')
-  mapElementRef!: ElementRef;
-
-  public map: any = null;
-
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
-
-  async ngAfterViewInit(): Promise<void> {
-    // Check if the code is running in the browser
-    if (isPlatformBrowser(this.platformId)) {
-      // Dynamically import Leaflet only in the browser
-      const L = await import('leaflet');
-
-      // Initialize the map
-      this.map = L.map(this.mapElementRef.nativeElement)
-        .setView([46.801111, 8.226667], 8);
-
-      // Add the tile layer
-      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; OpenStreetMap'
-      }).addTo(this.map);
-    }
-  }
+export class ContactComponent {
+  // No Leaflet-related code is needed
 }
