a6313bede4ab5129199241369343fe8ad06fc41b
stmiinfo <stmi.informatique@gmail.com>
2025-03-19 10:07:43 +0100

    emailjs added

--- src/app/devis/devis.component.ts
+++ src/app/devis/devis.component.ts
@@ -1,11 +1,53 @@
 import { Component } from '@angular/core';
+import emailjs from 'emailjs-com';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-devis',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './devis.component.html',
-  styleUrl: './devis.component.css'
+  styleUrls: ['./devis.component.css']
 })
 export class DevisComponent {
+  formData = {
+    name: '',
+    email: '',
+    phone: '',
+    produit: '',
+    message: '',
+    description: '',
+  };
 
+  // Method for form submission
+  onSubmit() {
+    const { name, email, phone, produit, message, description } = this.formData;
+
+    // Send email via EmailJS
+    const templateParams = {
+      name,
+      email,
+      phone,
+      produit,
+      message,
+      description
+    };
+
+    emailjs.send('service_enc84aj', 'template_z2z27vi', templateParams, 'CVBoTiOGk66Eq8jCp')
+      .then((response) => {
+        console.log('Email sent successfully', response);
+      })
+      .catch((error) => {
+        console.log('Failed to send email', error);
+      });
+
+    // Optionally, reset the form data after submission
+    this.formData = {
+      name: '',
+      email: '',
+      phone: '',
+      produit: '',
+      message: '',
+      description: '',
+    };
+  }
 }
